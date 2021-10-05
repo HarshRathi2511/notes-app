3ed2450a6b6d753204e24cc9d07d15742f443dd0
harsh <rathiharsh2002@gmail.com>
2021-10-05 11:59:14 +0530

    importing exporting files

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
-const fs = require('fs') //to import the fs module  
+// const fs = require('fs') //to import the fs module  
 //fs variable contains the stuff returned by 'fs' 
 
-fs.writeFileSync('notes.txt','My name is Harsh!') //a notes.txt file is created after this 
+// fs.writeFileSync('notes.txt','My name is Harsh!') //a notes.txt file is created after this 
 
 //To append a message to notes.txt
-fs.appendFileSync('notes.txt',' I\'m a pro node js developer')
+// fs.appendFileSync('notes.txt',' I\'m a pro node js developer
+
+
+// const firstName = require('./utils.js')  //this file gets executed first 
+
+// const name = 'Harsh'
+// console.log(firstName)
+
+// const addNumbers = require('./utils.js')
+
+// const sum = addNumbers(4,5)
+
+// console.log(sum)
+
+const getNotes = require('./notes.js')
+
+const message=  getNotes()
+
+console.log(message)
---
+++ utils.js
@@ -0,0 +1,12 @@
+//contains utilities 
+console.log('utils.js')
+
+const name ='Harsh'
+
+const add = function(a,b){
+    return a+b
+}
+
+// module.exports = name //so that it is exported and other files now can access it 
+
+module.exports = add
