7a39bbb343df7fba309a10d90640a8a3d7f53314
harsh <rathiharsh2002@gmail.com>
2021-10-07 16:14:10 +0530

    chalk library npm

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-const validator = require('validator')
-
+// const validator = require('validator')
+const chalk = require('chalk')
 const getNotes = require('./notes.js')
 
 const message=  getNotes()
 
 console.log(message)
 
-console.log(validator.isURL('https://google.com')) //=>true
+// console.log(validator.isURL('https://google.com')) //=>true
+
+//npm install chalk@2.4.1
+
+console.log(chalk.green.bold.inverse("Success!"))
