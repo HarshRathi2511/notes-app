c0050602b33fd9e8e9aff2a25c096a88737e7b33
harsh <rathiharsh2002@gmail.com>
2021-10-08 10:45:43 +0530

    using yargs to parse command line arguments and all commands customized

--- app.js
+++ app.js
@@ -1,19 +1,50 @@
-// const validator = require('validator')
-const chalk = require('chalk')
-const getNotes = require('./notes.js')
+const chalk = require("chalk");
+const { describe } = require("yargs");
+const yargs = require("yargs");
+//Yargs gives you:
+// commands and (grouped) options (my-program.js serve --port=5000).
+// a dynamically generated help menu based on your arguments:
+const getNotes = require("./notes.js");
 
-// const message=  getNotes()
-const command = process.argv[2]
+//Customize yargs version
+yargs.version("1.1.0");
+//add,remove,read,list all notes =>COMMANDS
 
-if(command=='add') {
-    console.log('Adding note')
-} else {
-    console.log('Note removed')
-}
+//Create ADD COMMAND
+yargs.command({
+  //verify by running node app.js --help
+  command: "add",
+  describe: "Add a new note",
+  handler: function () {
+    console.log("Adding a new note!");
+  }, //a function which will be passed the parsed argv.
+})
 
-// console.log(message)
+//Create REMOVE COMMAND 
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler:function() {
+        console.log('Removing a note !')
+    }
+})
 
-// console.log(chalk.green.bold.inverse("Success!"))
+//Create LIST COMMAND 
+yargs.command({
+    command: 'list',
+    describe: 'List all the notes',
+    handler:function() {
+        console.log('List all the notes !')
+    }
+})
 
-console.log(process.argv)   //array that contains all the arguments(exec file,file that is running,Arg passed via command line) provided by the command line 
-console.log(process.argv[2])
+//Create READ COMMAND 
+yargs.command({
+    command: 'read',
+    describe: 'Read all the notes',
+    handler:function() {
+        console.log('Read all the notes !')
+    }
+})
+
+console.log(yargs.argv); //{ _: [ 'add' ], title: 'Things to fuck', '$0': 'app.js' }
